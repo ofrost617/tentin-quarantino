6448ac0a6d4096f9b42b6be2ad57588d89476ace
Olivia Frost <oliviafrost@Olivias-MacBook-Pro.local>
2020-03-20 12:05:12 +0000

    Set up spring app with entry point for react app

---
+++ src/main/java/com/deliverydashboard/deliverydashboard/DeliveryDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.deliverydashboard.deliverydashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliverydashboard/deliverydashboard/DeliveryDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverydashboard.deliverydashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
